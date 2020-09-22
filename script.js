36f7bf26ebcbe55c3e574926ca3535ad4571c9f7
Tanner Kirkpatrick <twk4491@gmail.com>
2020-09-22 10:46:43 -0400

    Added comments and cleaned up code

--- script.js
+++ script.js
@@ -10,17 +10,12 @@ $(document).ready(function(){
 
     
 
-   //BACKGROUND COLORS FOR CURRENT HOUR//
+   //Color coded time blocks//
 
    function checkTime(){
        
         var currentHour = moment().format("HH");
-        
-        
-
-        console.log(currentHour);
-        
-
+      
         $(".timeBlock").each(function(){
 
             var timeBlock = parseInt($(this).children("td").attr("id"));
@@ -36,55 +31,34 @@ $(document).ready(function(){
                 $(this).addClass("future")
             }
         })
-
- 
-
    }
 
-   
+    checkTime();
 
-checkTime();
+   //setting each timeblock to local storage on save button click
 
-   
-
-
-   
-   
    $(".save").on("click", function(event){
        event.preventDefault();
        var value = $(this).parent().parent().siblings("td").children().val();
        var key = $(this).parent().parent().siblings("td").attr("id");
        console.log(key, value);
        localStorage.setItem(key, value);
-       showToast();
        
-
- 
+       showToast();
+   
    });
 
+   //toast notification to inform user that item has been saved
    function showToast(){
     var toastDiv = $("<p>").text("Item successfully saved");
-
     var check = $("<i>").attr("class", "fas fa-check");
-
     toastDiv.append(check);
-
     toastDiv.addClass("notify check fade fade-out");
-
     var container = $("#toastContainer");
-
-    
-
     container.html(toastDiv);
 
-
-
-
 }
-
-
-
-   
+    //Displaying saved items so content persists on reload
    $("#9 .text-field").val(localStorage.getItem("9"));
    $("#10 .text-field").val(localStorage.getItem("10"));
    $("#11 .text-field").val(localStorage.getItem("11"));
@@ -95,78 +69,25 @@ checkTime();
    $("#16 .text-field").val(localStorage.getItem("16"));
    $("#17 .text-field").val(localStorage.getItem("17"));
 
-   
+   //On trash button click, item will be cleared from input field and removed from local storage
    $(".trash").on("click", function(event){
       
       $(this).parent().parent().siblings("td").children().val("");
-
       localStorage.removeItem($(this).parent().parent().siblings("td").attr("id"));
-
+      
       showClear();
 
    })
 
+   //toast notification to inform user that item has been cleared
    function showClear(){
     var clearDiv = $("<p>").text("Item successfully cleared");
-
     var trash = $("<i>").attr("class", "far trash-can fa-trash-alt");
-
     clearDiv.append(trash);
-
-
     clearDiv.addClass("clear fade fade-out");
-
     var container = $("#toastContainer");
-
-
     container.html(clearDiv);
    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
